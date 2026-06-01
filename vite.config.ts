32b6c1eac3ac00de2ccf0bcb86493f31db2cd25d
Hassan9977 <hassanibrahim.t123@gmail.com>
2026-06-01 16:08:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Im Dev-Modus: Anfragen an /api werden ans Backend weitergeleitet
       '/api': {
-        target: 'https://bookmarks-backend-w11a.onrender.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
